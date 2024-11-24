e461ccb23d27f47203931026e11739849034ce58
Mian Muhammad Bodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2024-11-24 20:35:26 +0000

    withoutimage

--- UI/app.py
+++ UI/app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from PIL import Image
-import base64
-import io
 import add
 import sub
 import mult
@@ -10,26 +7,8 @@ import divid
 # Set the page config as the very first Streamlit command
 st.set_page_config(page_title="My Calculator", page_icon=":wave:", layout="wide")
 
-def get_image_base64(image_path):
-    """Encodes an image to base64 for embedding."""
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode()
-
-def display_round_image(image_path):
-    """Displays an image with rounded borders."""
-    try:
-        image = Image.open(image_path)
-        buffered = io.BytesIO()
-        image.save(buffered, format="JPEG")
-        img_str = base64.b64encode(buffered.getvalue()).decode()
-        st.markdown(
-            f'<img src="data:image/jpeg;base64,{img_str}" class="img-fluid" style="border-radius: 50%; width: 150px; height: 150px; object-fit: cover; display: block; margin-left: auto; margin-right: auto; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">',
-            unsafe_allow_html=True
-        )
-    except FileNotFoundError:
-        st.error("Error: Image file not found. Please check the path and try again.")
-
 def main():
+    """ Main function to navigate the app. """
     st.sidebar.title("Navigation")
     choice = st.sidebar.selectbox("Go to", ["Calculator", "About Me"])
 
@@ -39,7 +18,7 @@ def main():
         run_about_me()
 
 def run_calculator():
-    """Function to run the calculator logic."""
+    """ Runs calculator logic. """
     st.title("Simple Calculator")
     num1 = st.number_input("Enter First Number", format="%f")
     num2 = st.number_input("Enter Second Number", format="%f")
@@ -57,15 +36,13 @@ def run_calculator():
         st.success(f"The result is {result}")
 
 def run_about_me():
-    """Function to run the About Me page."""
+    """ Displays the 'About Me' section. """
     st.subheader("Hello! I'm Muhammad")
     st.write("""
     I'm a Certified Cloud Applied Generative AI Engineer specializing in Full Stack Web Development 
     with a focus on MicroServices and GPT integration. I develop Web 2.0 applications with custom GPTs 
     and chatbots, and have experience with Smart Contracts in Generative AI.
     """)
-
-    display_round_image("Portfolio-portrait-.jpeg")
 
     st.subheader("Watch My Short Film")
     video_url = "https://www.youtube.com/embed/QW9yOYjvlWc"
@@ -97,12 +74,3 @@ def run_about_me():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
